25506c86d9183f2e90b3e92891751b99606bb50e
imanurag17 <anuraag.cas@gmail.com>
2025-09-12 17:16:50 +0530

    node application with mongoDB

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const User = require('./models/user')
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('68c3de7d85564af913d5d70f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(4000);
+});
---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongodb = require('mongodb')
+
+const getDb = require('../util/database').getDb
+
+class User {
+  constructor(username, email){
+    this.name = username,
+    this.email = email
+  }
+
+  save(){
+    const db = getDb()
+    return db.collection('users').insertOne(this)
+  }
+
+  static findById(userId){
+    const db = getDb()
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
+  }
+}
+
+module.exports = User;
